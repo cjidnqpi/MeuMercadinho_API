883feb1eeaa57e54e770eff5ac53af0a6d23f3d5
julien <julien.marland@telecomnancy.eu>
2025-05-27 14:31:28 -0300

    ajout du type dans registerPro

--- registerPro.cjs
+++ registerPro.cjs
@@ -8,7 +8,7 @@ app.use(express.json());
 
 // Route POST pour registerPro
 router.post('/', (req, res) => {
-  const { email, password, name, cnpj, description } = req.body;
+  const { email, password, name, cnpj, description, type } = req.body;
 
   db.get('SELECT * FROM users WHERE email = ?', [email], (err, row) => {
     if (err) return res.status(500).json({ success: 0, error: err.message });
@@ -16,9 +16,12 @@ router.post('/', (req, res) => {
     db.get('SELECT * FROM waiting_line WHERE email = ?', [email], (err, row) => {
       if (err) return res.status(500).json({ success: 0, error: err.message });
       if (row) return res.json({ success: 2, message: 'Utilisateur en attente de validation' });
-      const stmt = db.prepare("INSERT INTO waiting_line (email, password, name,cnpj, description) VALUES (?, ?, ?, ?, ?)");
+      if (type !== 0 && type !== 1) {
+        return res.status(400).json({ success: 0, message: 'Type d utilisateur invalide' });
+      }
+      const stmt = db.prepare("INSERT INTO waiting_line (email, password, name, cnpj, description, type) VALUES (?, ?, ?, ?, ?, ?)");
       const hash = bcrypt.hashSync(password, saltRounds);
-      stmt.run(email, hash, name,cnpj, description);
+      stmt.run(email, hash, name, cnpj, description, type);
       stmt.finalize();
       res.json({ success: 3, message: 'Utilisateur ajouté à la file d attente avec succès' });
     });
